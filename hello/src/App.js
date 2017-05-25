084b8cd2b94a65b8cee43b9183066d0c07dd972e
Silot <aposnaml@teiser.gr>
2017-05-26 01:09:08 +0300

    react.js

---
+++ hello/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+  	projects: [
+    ]
+   }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+    		title: "Buisness Website",
+    		category: "Web design"
+    	},
+    	{
+    		title: "Social App",
+                    category: "Mobile Dev"
+
+    	},
+      {
+                    title: "Buisness Wensite",
+                    category: "Web Development"
+
+      }
+    ]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        My App
+        <Projects projects={this.state.projects}/>
+      </div>
+    );
+  }
+}
+
+export default App;
